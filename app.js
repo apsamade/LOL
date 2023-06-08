9b2e64be75e1a219c066b532eb87313ddf70e46d
bouderga abdel-samade <115378191+apsamade@users.noreply.github.com>
2023-06-09 00:10:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const adminRoutes = require('./routes/admin')
 const errController = require('./controller/err')
 const app = express();
 const path = require('path')
-
+const port = 3000
 
 app.set('view engine', 'ejs')
 app.use(express.static(path.dirname('public')))
 
 app.use(adminRoutes)
 app.use(errController.getErr)
-app.listen(3000)
+app.listen(port)
